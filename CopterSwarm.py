5bad44f3c39f11eace43f26a041e7835677ebdc4
rosrunner <luca.alfano@uni-a.de>
2024-03-18 15:18:38 +0100

    added charging

--- CopterSwarm.py
+++ CopterSwarm.py
@@ -62,8 +62,7 @@ class CopterSwarm(AbstractVirtualCapability):
                     formatPrint(self, f"Loading Copter: {copter.ood_id}")
                     self.__locks[i].acquire()
                     copter.invoke_sync("SetPosition", self.charging_station.invoke_sync("GetPosition", {}))
-                    copter.invoke_sync("SetBatteryChargeLevel", {"BatteryChargeLevel": 100.0})
-                    self.__locks[i].release()
+                    self.charging_station.invoke_async("ChargeDevice", {"Device": copter}, lambda: self.__locks[i].release())
         sleep(15)
 
 
