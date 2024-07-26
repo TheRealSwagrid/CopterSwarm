b5e6213979af719d7742f9eac3e50bab90055844
Luca Alfano <luca.alfano@t-online.de>
2024-07-26 03:21:32 +0200

    update from lappy

--- CopterSwarm.py
+++ CopterSwarm.py
@@ -21,28 +21,36 @@ class CopterSwarm(AbstractVirtualCapability):
         self.uri = "CopterSwarm"
         self.copters: List[SubDeviceRepresentation] = []
         self.__locks: List[Lock] = []
+        self.__loading_locks: List[Lock] = []
         self.charging_station: SubDeviceRepresentation = None
         self.initialized = False
 
     def AddCopter(self, params: dict):
         self.copters.append(json.loads(params["Device"]))
         self.__locks.append(Lock())
+        self.__loading_locks.append(Lock())
         return {"DeviceList": self.copters}
 
     def GetAvaiableCopter(self, params: dict):
         while self.running and len(self.copters) > 0:
             for i, l in enumerate(self.__locks):
                 if not l.locked():
+                    #if float(self.copters[i].invoke_sync("GetBatteryChargeLevel", {})["BatteryChargeLevel"]) < 25.:
+                        #continue
                     l.acquire()
-                    formatPrint(self, f"DEBUG: Transferring copter {i} with Battery: " + str(self.copters[i].invoke_sync("GetBatteryChargeLevel", {})["BatteryChargeLevel"]))
+                    # formatPrint(self, f"DEBUG: Transferring copter {i} with Battery: " + str(self.copters[i].invoke_sync("GetBatteryChargeLevel", {})["BatteryChargeLevel"]))
                     return {"Device": self.copters[i]}
         raise ValueError("No copter in this swarm!")
 
     def FreeCopter(self, params: dict):
-        copter = SubDeviceRepresentation(params["Device"], self, None)
+        copter_param = params["Device"]
+        if isinstance(copter_param, str):
+            copter_param = json.loads(copter_param)
+        copter = SubDeviceRepresentation(copter_param, self, None)
         for i, c in enumerate(self.copters):
             if copter.ood_id == c.ood_id and copter.json["requirements"] == c.json["requirements"]:
-                self.__locks[i].release()
+                if self.__locks[i].locked():
+                    self.__locks[i].release()
                 return {"Device": c}
         raise ValueError(f"Device not found {copter}")
 
@@ -52,19 +60,24 @@ class CopterSwarm(AbstractVirtualCapability):
         for i in range(count - len(self.copters)):
             self.copters.append(self.query_sync("VirtualCopter"))
             self.__locks.append(Lock())
+            self.__loading_locks.append(Lock())
         self.initialized = True
         return {"DeviceList": self.copters}
 
     def loop(self):
         if self.initialized:
             for i, copter in enumerate(self.copters):
-                battery_lvl = copter.invoke_sync("GetBatteryChargeLevel", {})["BatteryChargeLevel"]
+                battery_lvl = float(copter.invoke_sync("GetBatteryChargeLevel", {})["BatteryChargeLevel"])
                 if battery_lvl < 25.:
                     formatPrint(self, f"Loading Copter: {copter.ood_id}")
                     key = copy(i)
+                    if self.__loading_locks[key].locked():
+                        continue
+                    else:
+                        self.__loading_locks[key].acquire()
                     self.__locks[key].acquire()
                     copter.invoke_sync("SetPosition", self.charging_station.invoke_sync("GetPosition", {}))
-                    self.charging_station.invoke_async("ChargeDevice", {"Device": copter}, lambda *args: self.__locks[key].release())
+                    self.charging_station.invoke_async("ChargeDevice", {"Device": copter}, lambda *args: [self.__locks[key].release(), self.__loading_locks[key].release()])
         sleep(10)
 
 
